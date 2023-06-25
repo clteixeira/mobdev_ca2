c48627ddd573569652f9c1467b1d5f30b487a53c
clteixeira <127254606+clteixeira@users.noreply.github.com>
2023-06-25 22:06:43 +0100

    Weather App
    Weather

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: 'f938758fcd577ae5718d9e14a59b3a0f',
+  API_URL: 'https://api.openweathermap.org/data/2.5/'
 };
 
 /*
