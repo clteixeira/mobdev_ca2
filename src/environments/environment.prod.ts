08be76ba40356ea0c565190b0a9dacbf5e529d20
clteixeira <127254606+clteixeira@users.noreply.github.com>
2023-06-27 22:18:07 +0100

    Hidding Env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   API_KEY: '',
-  API_URL: 'https://api.openweathermap.org/data/2.5/'
+  API_URL: 'https://api.openweathermap.org/data/2.5/',
+  API_KEY_NEWS: '',
+  API_URL_NEWS: 'https://newsapi.org/v2/'
 };
